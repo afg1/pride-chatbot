3ee4e60d58f76bc820e142141369ca10bdfc0186
shabai517 <shabai517@163.com>
2023-06-13 15:57:22 +0100

    update main.py
    1. remove redundant code
    2. update the directory for LLM
    3. update the directory for embedding model

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import KonwledgeBaseQa  # class for search knowledge in database
+# import KonwledgeBaseQa  # class for search knowledge in database
 from langchain.chains import LLMChain  # A tool which use LLM in langchain
 from langchain.llms import HuggingFacePipeline # a tool load model in huggingface by pipline（vicuna）
 from langchain.embeddings import HuggingFaceEmbeddings, SentenceTransformerEmbeddings # Use to load the embedding model in hugginface
 from langchain.vectorstores import Chroma  #A tool that converts documents into vectors and can store and read them
 from langchain.docstore.document import Document # a function use to change chunk of the document into a format acceptable to chroma
 from langchain.prompts import PromptTemplate  # Tool for generating prompts
-from langchain.memory import ConversationBufferMemory  #tool for loading conversation history in prompt
-from langchain.chains import ConversationalRetrievalChain #tool for loading conversation history in prompt
+# from langchain.memory import ConversationBufferMemory  #tool for loading conversation history in prompt
+# from langchain.chains import ConversationalRetrievalChain #tool for loading conversation history in prompt
 from flask import Flask, request, jsonify #flask is a web server framework
 from flask_cors import CORS # allow cross domain request
 from transformers import AutoModelForCausalLM, AutoTokenizer,AutoModel  # tool for loading model from huggingface 
@@ -15,9 +15,9 @@ os.environ["TOKENIZERS_PARALLELISM"] = "ture"  #Load the environment variables r
 
 
 #Load the Tokenizer, convert the text input into an input that the model can accept
-tokenizer = AutoTokenizer.from_pretrained("/root/autodl-tmp/chatglm-6b", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("/hps/nobackup/juan/pride/chatbot/chatglm-6b", trust_remote_code=True)
 #Load the model, load it to the GPU in half-precision mode
-model = AutoModel.from_pretrained("/root/autodl-tmp/chatglm-6b", trust_remote_code=True).half().cuda()  
+model = AutoModel.from_pretrained("/hps/nobackup/juan/pride/chatbot/chatglm-6b", trust_remote_code=True).half().cuda()  
 
 
 
@@ -26,7 +26,7 @@ def vector_by_id(path_id: str):
     #Set the path of the database
     directory = "./vector/" + path_id
     #Load private knowledge base, uses embedding model named sentence-transformers
-    vector = Chroma(persist_directory=directory, embedding_function=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2"))
+    vector = Chroma(persist_directory=directory, embedding_function=HuggingFaceEmbeddings(model_name="/hps/nobackup/juan/pride/chatbot/all-MiniLM-L6-v2"))
     return vector
 
 #According to the query entered by the user, retrieve relevant documents in the private database (vector), and generate a complete prompt
