327b190922892ab00ef9323649eae6dad8413155
ypriverol <ypriverol@gmail.com>
2023-06-15 16:49:14 +0100

    major changes

--- chatbotcli.py
+++ chatbotcli.py
@@ -14,7 +14,7 @@ clear_command = 'cls' if os_name == 'Windows' else 'clear'
 stop_stream = False
 
 
-def llm_model_init(model_path: str, gpu: bool) -> (AutoTokenizer, AutoModel):
+def llm_model_init(model: str, gpu: bool) -> (AutoTokenizer, AutoModel):
     """
     Init model and tokenizer from provided path
     :param gpu: If use gpu to load model
@@ -23,28 +23,25 @@ def llm_model_init(model_path: str, gpu: bool) -> (AutoTokenizer, AutoModel):
     """
     os.environ["TOKENIZERS_PARALLELISM"] = "ture"  # Load the environment variables required by the local model
 
-    abs_path = os.path.abspath(model_path)
-
     # Load the Tokenizer, convert the text input into an input that the model can accept
-    tokenizer = AutoTokenizer.from_pretrained(abs_path, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True)
 
     # Load the model, load it to the GPU in half-precision mode
     if gpu:
-        model = AutoModel.from_pretrained(model_path,trust_remote_code=True).half().cuda()
+        model = AutoModel.from_pretrained(model,trust_remote_code=True).half().cuda()
     else:
         os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-        model = AutoModel.from_pretrained(model_path, trust_remote_code=True).cpu().float()
+        model = AutoModel.from_pretrained(model, trust_remote_code=True).cpu().float()
 
     return tokenizer, model
 
 
 # Load the specified private database (vector) by specifying the id
-def vector_by_id(path_id: str, model_path: str) -> Chroma:
+def vector_by_id(path_id: str, model: str) -> Chroma:
     # Set the path of the database
     directory = "./vector/" + path_id
-    abs_path = os.path.abspath(model_path)
     # Load private knowledge base, uses embedding model named sentence-transformers
-    vector = Chroma(persist_directory=directory, embedding_function=HuggingFaceEmbeddings(model_name=abs_path))
+    vector = Chroma(persist_directory=directory, embedding_function=HuggingFaceEmbeddings(model_name=model))
     return vector
 
 # According to the query entered by the user, retrieve relevant documents in the private database (vector),
@@ -89,7 +86,7 @@ def signal_handler(signal, frame):
     stop_stream = True
 
 
-def main(model: AutoModelForCausalLM, tokenizer: AutoTokenizer):
+def main(model, tokenizer):
     history = []
     global stop_stream
     print("PRIDE ChatGLM-6B，clear to Clean the history, stop to exit the program")
@@ -127,6 +124,6 @@ if __name__ == '__main__':
         cfg = yaml.load(ymlfile, Loader=yaml.Loader)
     for llm in cfg:
         print(llm)
-    tokenizer, model = llm_model_init(cfg['llm']['model_path'], cfg['llm']['gpu'])
-    vector = vector_by_id(cfg['vector']['uui'], cfg['llm']['model_path'])
+    tokenizer, model = llm_model_init(cfg['llm']['model'], cfg['llm']['gpu'])
+    vector = vector_by_id(cfg['vector']['uui'], cfg['llm']['model'])
     main(model, tokenizer) # call main function
