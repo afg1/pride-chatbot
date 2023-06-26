cb9fba49e9128b8313409be242e4c3fc477954ba
shabai517 <shabai517@gmail.com>
2023-06-26 15:46:09 +0100

    update chatbotcli
    1. add gpt4all LLM
    2. add llm_chat function to handle all the LLM model reply
    3. add the choice for LLM in command line

--- chatbotcli.py
+++ chatbotcli.py
@@ -1,6 +1,6 @@
 
 import platform
-
+import gpt4all
 import yaml
 from langchain.embeddings import HuggingFaceEmbeddings, SentenceTransformerEmbeddings  # Use to load the embedding model in hugginface
 from langchain.vectorstores import Chroma  # A tool that converts documents into vectors and can store and read them
@@ -14,27 +14,40 @@ clear_command = 'cls' if os_name == 'Windows' else 'clear'
 stop_stream = False
 os.environ["TOKENIZERS_PARALLELISM"] = "ture"  # Load the environment variables required by the local model
 
-def llm_model_init(model: str, gpu: bool) -> (AutoTokenizer, AutoModel):
+def llm_model_init(choice: str, gpu: bool) -> (AutoTokenizer, AutoModel):
     """
     Init model and tokenizer from provided path
     :param gpu: If use gpu to load model
     :param model_path: model path
     :return: tokenizer, model
     """
+    with open("config.yml", "r") as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.Loader)
+    if choice =='1':  #load ChatGLM-6B
+        model_path = cfg['llm']['chatglm']
+        # Load the Tokenizer, convert the text input into an input that the model can accept
+        tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        # Load the model, load it to the GPU in half-precision mode
+        if gpu:
+            model = AutoModel.from_pretrained(model,trust_remote_code=True).half().cuda()
+        else:
+            os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+            model = AutoModel.from_pretrained(model, trust_remote_code=True).cpu().float()
+        return tokenizer, model
+    elif choice == '2': #load the GPT4All only cpu
+        model_path = cfg['llm']['GPT4ALL_PATH']
+        model_name = cfg['llm']['GPTEALL_MODEL']
+        model = gpt4all.GPT4All(model_path=model_path, model_name=model_name)
+        return '1', model
 
-
-    # Load the Tokenizer, convert the text input into an input that the model can accept
-    tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True)
-
-    # Load the model, load it to the GPU in half-precision mode
-    if gpu:
-        model = AutoModel.from_pretrained(model,trust_remote_code=True).half().cuda()
-    else:
-        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-        model = AutoModel.from_pretrained(model, trust_remote_code=True).cpu().float()
-
-    return tokenizer, model
-
+#chat with model    
+def llm_chat(choice:str,prompt:str,tokenizer,model):
+    if choice =='1':#chat with ChatGLM
+        result = model.chat(tokenizer,prompt, history=[])
+    elif choice =='2':#chat with GPT4ALL 
+        messages = [{"role": "user", "content": prompt}]
+        result = model.chat_completion(messages,default_prompt_header=False)
+    return result
 
 # Load the specified private database (vector) by specifying the id
 def vector_by_id(database_path: str, model: str) -> Chroma:
@@ -123,10 +136,10 @@ def signal_handler(signal, frame):
     stop_stream = True
 
 
-def main(model, tokenizer, vector):
+def main(choice:str, model, tokenizer, vector):
     history = []
     global stop_stream
-    print("PRIDE ChatGLM-6B，clear to Clean the history, stop to exit the program")
+    print("PRIDE Chatbot，clear to Clean the history, stop to exit the program")
     while True:
         query = input("\nQuestion：")
         if query.strip() == "stop":
@@ -139,9 +152,12 @@ def main(model, tokenizer, vector):
         count = 0
         prompt,docs = get_similar_answer(vector, query)
         print(docs)
-        result = model.chat(tokenizer,prompt, history=[])
+        result = llm_chat(choice, prompt, tokenizer, model)
         print('Answer:')
-        print(result[0])
+        if choice =='1':
+            print(result[0])
+        else:
+            print(result)
 
 
         # for response, history in result:
@@ -163,7 +179,9 @@ if __name__ == '__main__':
         cfg = yaml.load(ymlfile, Loader=yaml.Loader)
     for llm in cfg:
         print(llm)
-    tokenizer, model = llm_model_init(cfg['llm']['model'], cfg['llm']['gpu'])
-    database_path = cfg['vector']['cli_store'] + cfg['vector']['uui'] + "/"
+    print("Please choose the model!\n 1 - chatglm \n 2 - GPT4ALL")
+    choice = input('\nChoice:') #user choose model with number 
+    tokenizer, model = llm_model_init(choice.strip(), cfg['llm']['gpu'])
+    database_path = cfg['vector']['cli_store'] + cfg['vector']['uui'] 
     vector = vector_by_id(database_path=database_path,model=cfg['llm']['embedding'])
-    main(model, tokenizer, vector) # call main function
+    main(choice, model, tokenizer, vector) # call main function
