1d3e1cb40982a992b5e64a74e7e0daab07a5c67a
shabai517 <shabai517@gmail.com>
2023-06-30 15:40:58 +0100

    remove the structure loader

--- build_indexer.py
+++ build_indexer.py
@@ -14,43 +14,9 @@ from langchain.document_loaders.unstructured import UnstructuredFileLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.docstore.document import Document
+from structured import StructuredMarkdownLoader
 
 os.environ["NLTK_DATA"] = '/hps/nobackup/juan/pride/chatbot/'
-
-
-class StructuredMarkdownLoader(UnstructuredFileLoader):
-    
-    def _get_elements(self) -> List:
-        from unstructured.__version__ import __version__ as __unstructured_version__
-        from unstructured.partition.md import partition_md
-
-        # NOTE(MthwRobinson) - enables the loader to work when you're using pre-release
-        # versions of unstructured like 0.4.17-dev1
-        _unstructured_version = __unstructured_version__.split("-")[0]
-        unstructured_version = tuple([int(x) for x in _unstructured_version.split(".")])
-
-        if unstructured_version < (0, 4, 16):
-            raise ValueError(
-                f"You are on unstructured version {__unstructured_version__}. "
-                "Partitioning markdown files is only supported in unstructured>=0.4.16."
-            )
-        filename = self.file_path 
-        docs = []
-        if filename.endswith(".md"):
-            path = Path(filename)  
-            content = path.read_text()
-            sections = self.extract_sections(content)
-            for section in sections:
-                new_doc = Document(page_content=section.strip())
-                docs.append(new_doc)
-        return docs 
-    
-    @staticmethod
-    def extract_sections(content: str) -> list:
-        pattern = r"\n## |\n### |\n#### |\Z"
-        sections = re.split(pattern, content)
-        sections = [s.strip() for s in sections if s.strip()]
-        return sections
 
 
 # Define the Chroma settings
