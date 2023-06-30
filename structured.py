1c958e91b5882f5f7dfb527f1af9e700b12c763c
shabai517 <shabai517@gmail.com>
2023-06-30 17:52:58 +0100

    remove the md type check
    it will cause the file read bug

--- structured.py
+++ structured.py
@@ -6,19 +6,19 @@ from langchain.docstore.document import Document
 
 class StructuredMarkdownLoader(UnstructuredFileLoader):
     def _get_elements(self) -> List:
-        from unstructured.__version__ import __version__ as __unstructured_version__
-        #from unstructured.partition.md import partition_md
+        # from unstructured.__version__ import __version__ as __unstructured_version__
+        # #from unstructured.partition.md import partition_md
 
-        # NOTE(MthwRobinson) - enables the loader to work when you're using pre-release
-        # versions of unstructured like 0.4.17-dev1
-        _unstructured_version = __unstructured_version__.split("-")[0]
-        unstructured_version = tuple([int(x) for x in _unstructured_version.split(".")])
+        # # NOTE(MthwRobinson) - enables the loader to work when you're using pre-release
+        # # versions of unstructured like 0.4.17-dev1
+        # _unstructured_version = __unstructured_version__.split("-")[0]
+        # unstructured_version = tuple([int(x) for x in _unstructured_version.split(".")])
         
-        if unstructured_version < (0, 4, 16):
-            raise ValueError(
-                f"You are on unstructured version {__unstructured_version__}. "
-                "Partitioning markdown files is only supported in unstructured>=0.4.16."
-            )
+        # if unstructured_version < (0, 4, 16):
+        #     raise ValueError(
+        #         f"You are on unstructured version {__unstructured_version__}. "
+        #         "Partitioning markdown files is only supported in unstructured>=0.4.16."
+        #     )
             
         filename = self.file_path 
         docs = []
