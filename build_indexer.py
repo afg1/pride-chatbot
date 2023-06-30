6e8dac7f4e498984d739bb9f5596a49fe7cea1b0
shabai517 <shabai517@gmail.com>
2023-06-30 15:08:02 +0100

    fix bug for load documents

--- build_indexer.py
+++ build_indexer.py
@@ -93,9 +93,10 @@ def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Docum
     with Pool(processes=os.cpu_count()) as pool:
         results = []
         with tqdm(total=len(filtered_files), desc='Loading new documents', ncols=80) as pbar:
-            for i, doc in enumerate(pool.imap_unordered(load_single_document, filtered_files)):
-                results.append(doc)
-                pbar.update()
+            for i, docs in enumerate(pool.imap_unordered(load_single_document, filtered_files)):
+                for doc in docs:
+                    results.append(doc)
+                    pbar.update()
 
     return results
 
