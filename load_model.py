974a132fa7a1d27c93f5ea3ef12cc7b164a40d19
shabai517 <shabai517@gmail.com>
2023-10-16 15:19:06 +0100

    refine load model code

--- load_model.py
+++ load_model.py
@@ -18,66 +18,12 @@ import torch.cuda as cuda
 import transformers
 from transformers import pipeline
 
-
-class Load_model:
-    """Model loader is a class for model load
-
-      Args: model_path
-    
-    """
-
-    kwargs = {}
-
-    def __init__(self,model_path) -> None:
-
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model_path = model_path
-        self.kwargs = {
-            "torch_dtype": torch.float16,
-            "device_map": "auto",
-        }
-
-    def loader(self, num_gpus, load_8bit=False, debug=False):
-        if self.device == "cpu":
-            kwargs = {}
-        elif self.device == "cuda":
-            kwargs = {"torch_dtype": torch.float16}
-            if num_gpus == "auto":
-                kwargs["device_map"] = "auto"
-            else:
-                num_gpus = int(num_gpus)
-                if num_gpus != 1:
-                    kwargs.update({
-                        "device_map": "auto",
-                        "max_memory": {i: "13GiB" for i in range(num_gpus)},
-                    })
-        else:
-            raise ValueError(f"Invalid device: {self.device}")
-
-        if "chatglm" in self.model_path:
-            tokenizer = AutoTokenizer.from_pretrained(self.model_path, trust_remote_code=True)
-            model = AutoModel.from_pretrained(self.model_path, trust_remote_code=True).half().cuda()
-        else:
-            tokenizer = AutoTokenizer.from_pretrained(self.model_path, use_fast=False)
-            model = AutoModelForCausalLM.from_pretrained(self.model_path,
-                                                         low_cpu_mem_usage=True, **kwargs)
-
-        # if load_8bit:
-        #     compress_module(model, self.device)
-
-        if (self.device == "cuda" and num_gpus == 1):
-            model.to(self.device)
-
-        if debug:
-            print(model)
-
-        return model, tokenizer
-
-    # Variables initialization
+# Variables initialization
 os_name = platform.system()
 clear_command = 'cls' if os_name == 'Windows' else 'clear'
 stop_stream = False
 os.environ["TOKENIZERS_PARALLELISM"] = "ture"  # Load the environment variables required by the local model
+
 def llm_model_init(choice: str, gpu: bool) :
     torch.cuda.empty_cache()
     cuda.empty_cache()
